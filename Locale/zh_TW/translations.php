bbca7e6ecbc2132c54505c63d1601de78b401f96
Kenneth-Ran (Aaron) <11792366+kenneth-ran@users.noreply.github.com>
2020-11-06 11:52:32 +0800

    Add translate file for zh_TW (Traditional Chinese)
    Translate for zh_TW (Traditional Chinese)
    for Hong Kong, Macau, Taiwan, Malaysia...

---
+++ Locale/zh_TW/translations.php
@@ -0,0 +1,18 @@
+<?php
+
+return array(
+    'Sort by position' => '按位置排序',
+    'Sort by date' => '按日期排序',
+    'Add task' => '增加任務',
+    'There is no start date or due date for this task.' => '目前任務無開始或結束日。',
+    'Moving or resizing a task will change the start and due date of the task.' => '搬移或重設任務，將會改變任務的開始與結束日。',
+    'There is no task in your project.' => '你的專案目前還沒有任務',
+    'Gantt chart' => '甘特圖',
+    'Gantt chart for all projects' => '所有專案的甘特圖',
+    'Gantt chart for this project' => '此專案的甘特圖',
+    'Project board' => '專案面板',
+    'There is no start date or end date for this project.' => '目前專案沒有開始或結束日',
+    'Projects Gantt chart' => '專案甘特圖',
+    'Switch to the Gantt chart view' => '切換到甘特圖視界',
+);
+
